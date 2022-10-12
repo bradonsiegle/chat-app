1daad5dab024c7b16058af07b30bd6a48dca311c
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-12 16:13:40 -0600

    add register functionality

--- backend/controllers/auth/postRegister.js
+++ backend/controllers/auth/postRegister.js
@@ -1,5 +1,41 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+
 const postRegister = async (req, res) => {
-	res.send('Register');
+	try {
+		const { username, email, password } = req.body;
+
+		// Check if user already exists
+		const alreadyExists = await User.findOne({ email: email.toLowerCase() });
+		if (alreadyExists) {
+			return res.status(409).send('Email address is already in use');
+		}
+
+		//Hash password
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// Create new user in database
+		const user = await User.create({
+			username,
+			email: email.toLowerCase(),
+			password: hashedPassword,
+		});
+
+		//Create JWT token
+		const token = 'JWT TOKEN';
+
+		// Send to client
+		res.status(201).json({
+			userDetails: {
+				username: user.username,
+				email: user.email,
+				token: token,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: 'Internal server error' });
+	}
 };
 
 module.exports = postRegister;
