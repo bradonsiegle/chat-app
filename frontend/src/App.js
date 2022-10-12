ce3be5e39c8fbccd7bdb3875b4f02e876f931299
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-12 17:34:16 -0600

    add router structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route exact path='/login'>
+						<LoginPage />
+					</Route>
+					<Route exact path='/register'>
+						<RegisterPage />
+					</Route>
+					<Route exact path='/dashboard'>
+						<Dashboard />
+					</Route>
+					<Route path='/'>
+						<Navigate to='/dashboard' />
+					</Route>
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
