81b58539534479deb93dd35dc883a0895e9e7a13
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 13:41:40 -0600

    add redirect

--- frontend/src/authPages/LoginPage/LoginPageFooter.jsx
+++ frontend/src/authPages/LoginPage/LoginPageFooter.jsx
@@ -1,12 +1,25 @@
-import { CustomPrimaryButton } from '../../shared/components/CustomPrimaryButton';
+import { CustomPrimaryButton, RedirectInfo } from '../../shared/components';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPageFooter = ({ handleLogin, isFormValid }) => {
+	const navigate = useNavigate();
+
 	return (
-		<CustomPrimaryButton
-			label='Login'
-			additionalStyles={{ marginTop: '2rem' }}
-			disabled={!isFormValid}
-			onClick={handleLogin}
-		/>
+		<>
+			<div>
+				<CustomPrimaryButton
+					label='Login'
+					additionalStyles={{ marginTop: '2rem' }}
+					disabled={!isFormValid}
+					onClick={handleLogin}
+				/>
+			</div>
+			<RedirectInfo
+				text="Don't have an account? "
+				redirectText='Sign up'
+				additionalStyles={{ marginTop: '0.5rem' }}
+				redirectHandler={() => navigate('/register')}
+			/>
+		</>
 	);
 };
