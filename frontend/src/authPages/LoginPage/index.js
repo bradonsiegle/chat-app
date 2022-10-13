6f8395b9634611be9d05a485367e981431031dea
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 13:12:48 -0600

    add index for login page exports

---
+++ frontend/src/authPages/LoginPage/index.js
@@ -0,0 +1,6 @@
+import { LoginPage } from './LoginPage';
+import { LoginPageFooter } from './LoginPageFooter';
+import { LoginPageHeader } from './LoginPageHeader';
+import { LoginPageInputs } from './LoginPageInputs';
+
+export { LoginPage, LoginPageFooter, LoginPageHeader, LoginPageInputs };
