b43860e05bdeec6293dd44d75d05bc73baf43d9d
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 14:14:43 -0600

    add register page structure

--- frontend/src/authPages/RegisterPage/RegisterPage.jsx
+++ frontend/src/authPages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
+import { Typography } from '@mui/material';
 import { AuthBox } from '../../shared/components/AuthBox';
+import { RegisterPageInputs } from './RegisterPageInputs';
 
 export const RegisterPage = () => {
-	return <AuthBox>RegisterPage</AuthBox>;
+	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [isFormValid, setIsFormValid] = useState(false);
+
+	return (
+		<AuthBox>
+			<Typography variant='h5' sx={{ color: '#fff' }}>
+				Create an account
+			</Typography>
+			<RegisterPageInputs
+				email={email}
+				setEmail={setEmail}
+				username={username}
+				setUsername={setUsername}
+				password={password}
+				setPassword={setPassword}
+				confirmPassword={confirmPassword}
+				setConfirmPassword={setConfirmPassword}
+			/>
+		</AuthBox>
+	);
 };
