4df44b6dc2a4e45cd84ba9b669fc03ca560a3ba6
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 14:02:25 -0600

    add form validation

--- frontend/src/authPages/LoginPage/LoginPage.jsx
+++ frontend/src/authPages/LoginPage/LoginPage.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AuthBox } from '../../shared/components/AuthBox';
 import { LoginPageHeader, LoginPageInputs, LoginPageFooter } from './index.js';
+import { validateLoginForm } from '../../shared/utils/validators';
 
 export const LoginPage = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [isFormValid, setIsFormValid] = useState(false);
 
+	useEffect(() => {
+		setIsFormValid(validateLoginForm({ email, password }));
+	}, [email, password, setIsFormValid]);
+
 	const handleLogin = () => {
-		console.log('Login');
+		console.log(email, password);
 	};
 
 	return (
