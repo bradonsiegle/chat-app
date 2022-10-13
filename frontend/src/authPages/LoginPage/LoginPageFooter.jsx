1ef810c60bed4ea152ddec5e97cadb9294bacb51
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 13:52:48 -0600

    add tooltip

--- frontend/src/authPages/LoginPage/LoginPageFooter.jsx
+++ frontend/src/authPages/LoginPage/LoginPageFooter.jsx
@@ -1,19 +1,34 @@
 import { CustomPrimaryButton, RedirectInfo } from '../../shared/components';
 import { useNavigate } from 'react-router-dom';
+import { Tooltip } from '@mui/material';
+
+const getFormMessage = (isFormValid) => {
+	if (isFormValid) {
+		return 'Login';
+	} else {
+		return 'Enter correct email address and password';
+	}
+};
 
 export const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 	const navigate = useNavigate();
 
 	return (
 		<>
-			<div>
-				<CustomPrimaryButton
-					label='Login'
-					additionalStyles={{ marginTop: '2rem' }}
-					disabled={!isFormValid}
-					onClick={handleLogin}
-				/>
-			</div>
+			<Tooltip
+				title={
+					<h3 style={{ fontSize: '0.8rem' }}>{getFormMessage(isFormValid)}</h3>
+				}
+			>
+				<div>
+					<CustomPrimaryButton
+						label='Login'
+						additionalStyles={{ marginTop: '2rem' }}
+						disabled={!isFormValid}
+						onClick={handleLogin}
+					/>
+				</div>
+			</Tooltip>
 			<RedirectInfo
 				text="Don't have an account? "
 				redirectText='Sign up'
