f15e9139fedc7675116cd3a398ca4b032ff7897c
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 13:31:38 -0600

    add export

--- frontend/src/authPages/LoginPage/index.js
+++ frontend/src/authPages/LoginPage/index.js
@@ -1,6 +1,5 @@
-import { LoginPage } from './LoginPage';
 import { LoginPageFooter } from './LoginPageFooter';
 import { LoginPageHeader } from './LoginPageHeader';
 import { LoginPageInputs } from './LoginPageInputs';
 
-export { LoginPage, LoginPageFooter, LoginPageHeader, LoginPageInputs };
+export { LoginPageFooter, LoginPageHeader, LoginPageInputs };
--- frontend/src/shared/components/index.js
+++ frontend/src/shared/components/index.js
@@ -1,5 +1,6 @@
 import { AuthBox } from './AuthBox';
 import { InputWithLabel } from './InputWithLabel';
 import { CustomPrimaryButton } from './CustomPrimaryButton';
+import { RedirectInfo } from './RedirectInfo';
 
-export { AuthBox, InputWithLabel, CustomPrimaryButton };
+export { AuthBox, InputWithLabel, CustomPrimaryButton, RedirectInfo };
