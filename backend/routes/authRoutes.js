47bbd854d08874c87d3622590bfad3720de4fd73
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 14:27:12 -0600

    change max username length to 16

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,7 +9,7 @@ const { postLogin, postRegister } = authControllers.controllers;
 
 //validate requests
 const registerSchema = Joi.object({
-	username: Joi.string().min(3).max(12).required(),
+	username: Joi.string().min(3).max(16).required(),
 	password: Joi.string().min(6).max(16).required(),
 	email: Joi.string().email().required(),
 });
