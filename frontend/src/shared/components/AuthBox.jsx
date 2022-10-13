4785ed450f3cb931be939bc0fbbf21039c5c60ea
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-12 18:11:02 -0600

    create component

---
+++ frontend/src/shared/components/AuthBox.jsx
@@ -0,0 +1,32 @@
+import Box from '@mui/material/Box';
+import { styled } from '@mui/system';
+
+const BoxWrapper = styled('div')({
+	width: '100%',
+	height: '100vh',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	backgroundColor: '#5865f2',
+});
+
+export const AuthBox = (props) => {
+	return (
+		<BoxWrapper>
+			<Box
+				sx={{
+					width: 700,
+					height: 400,
+					bgcolor: '#36393f',
+					borderRadius: '5px',
+					boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.2)',
+					display: 'flex',
+					flexDirection: 'column',
+					padding: '20px',
+				}}
+			>
+				{props.children}
+			</Box>
+		</BoxWrapper>
+	);
+};
