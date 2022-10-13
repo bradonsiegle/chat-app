bfa05da97fbcbbc8918e81f742ceed220b271832
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-13 14:27:59 -0600

    add username and confirmPassword

--- frontend/src/shared/utils/validators.js
+++ frontend/src/shared/utils/validators.js
@@ -2,6 +2,24 @@ export const validateLoginForm = ({ email, password }) => {
 	return validateEmail(email) && validatePassword(password);
 };
 
+export const validateRegisterForm = ({
+	username,
+	email,
+	password,
+	confirmPassword,
+}) => {
+	return (
+		validateUsername(username) &&
+		validateEmail(email) &&
+		validatePassword(password) &&
+		validateConfirmPassword(password, confirmPassword)
+	);
+};
+
+const validateUsername = (username) => {
+	return username.length >= 3 && username.length <= 16;
+};
+
 const validateEmail = (email) => {
 	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 	return emailRegex.test(String(email).toLowerCase());
@@ -9,4 +27,8 @@ const validateEmail = (email) => {
 
 const validatePassword = (password) => {
 	return password.length >= 6 && password.length <= 16;
+};
+
+const validateConfirmPassword = (password, confirmPassword) => {
+	return password === confirmPassword;
 };
