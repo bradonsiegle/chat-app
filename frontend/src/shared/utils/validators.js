49684437bbac2edd146ecb1851fabdd822569cd7
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 12:09:07 -0600

    change validator to return an object

--- frontend/src/shared/utils/validators.js
+++ frontend/src/shared/utils/validators.js
@@ -1,28 +1,57 @@
+import {
+	usernameLengthError,
+	passwordLengthError,
+	passwordMatchError,
+	emailError,
+} from './validatorErrors';
+
 export const validateLoginForm = ({ email, password }) => {
 	return validateEmail(email) && validatePassword(password);
 };
 
+//checks for valid username, email, password, and matching passwords
+//returns an object with the error messages and a boolean value for if the form is valid
 export const validateRegisterForm = ({
 	username,
 	email,
 	password,
 	confirmPassword,
 }) => {
-	return (
-		validateUsername(username) &&
-		validateEmail(email) &&
-		validatePassword(password) &&
-		validateConfirmPassword(password, confirmPassword)
-	);
+	const validatedForm = {
+		isValid: false,
+		error: '',
+	};
+	if (!validateUsername(username)) {
+		validatedForm.isValid = false;
+		validatedForm.error = usernameLengthError;
+		return validatedForm;
+	}
+	if (!validateEmail(email)) {
+		validatedForm.isValid = false;
+		validatedForm.error = emailError;
+		return validatedForm;
+	}
+	if (!validatePassword(password)) {
+		validatedForm.isValid = false;
+		validatedForm.error = passwordLengthError;
+		return validatedForm;
+	}
+	if (!validateConfirmPassword(password, confirmPassword)) {
+		validatedForm.isValid = false;
+		validatedForm.error = passwordMatchError;
+		return validatedForm;
+	}
+	validatedForm.isValid = true;
+	return validatedForm;
 };
 
 const validateUsername = (username) => {
-	return String(username).length >= 6 && String(username).length <= 16;
+	return username.length >= 3 && username.length <= 16;
 };
 
 const validateEmail = (email) => {
 	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-	return emailRegex.test(String(email).toLowerCase());
+	return emailRegex.test(String(email)?.toLowerCase());
 };
 
 const validatePassword = (password) => {
