c10a67fa07a742db69269a7f0b347441690d86a7
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 13:48:42 -0600

    add redux hooks

--- frontend/src/authPages/LoginPage/LoginPage.jsx
+++ frontend/src/authPages/LoginPage/LoginPage.jsx
@@ -2,8 +2,14 @@ import { useState, useEffect } from 'react';
 import { AuthBox } from '../../shared/components/AuthBox';
 import { LoginPageHeader, LoginPageInputs, LoginPageFooter } from './index.js';
 import { validateLoginForm } from '../../shared/utils/validators';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../store/actions/authActions';
 
 export const LoginPage = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [isFormValid, setIsFormValid] = useState(false);
@@ -13,7 +19,8 @@ export const LoginPage = () => {
 	}, [email, password, setIsFormValid]);
 
 	const handleLogin = () => {
-		console.log(email, password);
+		const userDetails = { email, password };
+		dispatch(login(userDetails, navigate));
 	};
 
 	return (
--- frontend/src/authPages/RegisterPage/RegisterPage.jsx
+++ frontend/src/authPages/RegisterPage/RegisterPage.jsx
@@ -3,8 +3,14 @@ import { Typography } from '@mui/material';
 import { AuthBox } from '../../shared/components/AuthBox';
 import { RegisterPageInputs, RegisterPageFooter } from './';
 import { validateRegisterForm } from '../../shared/utils/validators';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { register } from '../../store/actions/authActions';
 
 export const RegisterPage = () => {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
 	const [email, setEmail] = useState('');
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
@@ -22,7 +28,8 @@ export const RegisterPage = () => {
 		setIsFormValid(isValid);
 		setError(error);
 		if (isValid) {
-			console.log(email, username, password, confirmPassword);
+			const userDetails = { email, username, password };
+			dispatch(register(userDetails, navigate));
 		} else {
 			console.log(error);
 		}
