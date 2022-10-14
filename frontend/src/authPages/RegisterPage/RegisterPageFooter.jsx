6b3c5cf71449bbc0d6ab1957afcafa4b14e37ea3
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 15:23:00 -0600

    remove tooltip

--- frontend/src/authPages/RegisterPage/RegisterPageFooter.jsx
+++ frontend/src/authPages/RegisterPage/RegisterPageFooter.jsx
@@ -1,41 +1,16 @@
 import { CustomPrimaryButton, RedirectInfo } from '../../shared/components';
 import { useNavigate } from 'react-router-dom';
-import { Tooltip } from '@mui/material';
 
-const getFormMessage = (isFormValid, formError) => {
-	if (isFormValid) {
-		return 'Success!';
-	} else {
-		return formError;
-	}
-};
-
-export const RegisterPageFooter = ({
-	handleRegister,
-	isFormValid,
-	formError,
-}) => {
+export const RegisterPageFooter = ({ handleRegister }) => {
 	const navigate = useNavigate();
 
 	return (
 		<>
-			<Tooltip
-				title={
-					<h3 style={{ fontSize: '0.8rem' }}>
-						{getFormMessage(isFormValid, formError)}
-					</h3>
-				}
-				enterTouchDelay={0}
-				leaveTouchDelay={5000}
-			>
-				<div>
-					<CustomPrimaryButton
-						label='Register'
-						additionalStyles={{ marginTop: '2rem' }}
-						onClick={handleRegister}
-					/>
-				</div>
-			</Tooltip>
+			<CustomPrimaryButton
+				label='Register'
+				additionalStyles={{ marginTop: '2rem' }}
+				onClick={handleRegister}
+			/>
 			<RedirectInfo
 				text='Already have an account? '
 				redirectText='Sign in'
