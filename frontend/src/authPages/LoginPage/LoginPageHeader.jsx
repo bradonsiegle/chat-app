448c99995c05b769676c4aaade65a9357779f80f
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 15:22:25 -0600

    center text alignment

--- frontend/src/authPages/LoginPage/LoginPageHeader.jsx
+++ frontend/src/authPages/LoginPage/LoginPageHeader.jsx
@@ -3,10 +3,10 @@ import { Typography } from '@mui/material';
 export const LoginPageHeader = () => {
 	return (
 		<>
-			<Typography variant='h5' sx={{ color: 'white' }}>
+			<Typography variant='h5' sx={{ color: 'white', textAlign: 'center' }}>
 				Welcome back!
 			</Typography>
-			<Typography sx={{ color: '#b9bbbe' }}>
+			<Typography sx={{ color: '#b9bbbe', textAlign: 'center' }}>
 				We're so excited to see you again!
 			</Typography>
 		</>
