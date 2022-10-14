afa3fd64e4e6c9a05fb6196304a89b5bcfac0891
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 15:24:23 -0600

    fix name

--- frontend/src/store/reducers/alertReducer.js
+++ frontend/src/store/reducers/alertReducer.js
@@ -1,7 +1,7 @@
-import alertActions from '../actions/alertActions';
+import { alertActions } from '../actions/alertActions';
 
 const initialState = {
-	showAlert: false,
+	open: false,
 	message: '',
 };
 
@@ -10,12 +10,12 @@ const reducer = (state = initialState, action) => {
 	switch (type) {
 		case alertActions.SHOW_ALERT:
 			return {
-				showAlert: true,
+				open: true,
 				message: message,
 			};
 		case alertActions.HIDE_ALERT:
 			return {
-				showAlert: false,
+				open: false,
 				message: '',
 			};
 		default:
