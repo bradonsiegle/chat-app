8f03851adc3c9f2074c84357b0f87f652b582635
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 12:08:37 -0600

    add input form error responses

---
+++ frontend/src/shared/utils/validatorErrors.js
@@ -0,0 +1,6 @@
+export const usernameLengthError =
+	'Username must be between 3 and 16 characters';
+export const passwordLengthError =
+	'Password must be between 6 and 16 characters';
+export const passwordMatchError = 'Passwords do not match';
+export const emailError = 'Email address is not valid';
