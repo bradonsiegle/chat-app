75951f726634e10162f299f58fc1396d9536bc8b
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-14 15:22:11 -0600

    remove tooltip, fix styles

--- frontend/src/authPages/LoginPage/LoginPageFooter.jsx
+++ frontend/src/authPages/LoginPage/LoginPageFooter.jsx
@@ -1,10 +1,9 @@
 import { CustomPrimaryButton, RedirectInfo } from '../../shared/components';
 import { useNavigate } from 'react-router-dom';
-import { Tooltip } from '@mui/material';
 
 const getFormMessage = (isFormValid) => {
 	if (isFormValid) {
-		return 'Login';
+		return 'Log In';
 	} else {
 		return 'Enter correct email address and password';
 	}
@@ -15,24 +14,16 @@ export const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 
 	return (
 		<>
-			<Tooltip
-				title={
-					<h3 style={{ fontSize: '0.8rem' }}>{getFormMessage(isFormValid)}</h3>
-				}
-			>
-				<div>
-					<CustomPrimaryButton
-						label='Login'
-						additionalStyles={{ marginTop: '2rem' }}
-						disabled={!isFormValid}
-						onClick={handleLogin}
-					/>
-				</div>
-			</Tooltip>
+			<CustomPrimaryButton
+				label='Log In'
+				additionalStyles={{ marginTop: '2rem' }}
+				disabled={!isFormValid}
+				onClick={handleLogin}
+			/>
 			<RedirectInfo
 				text="Don't have an account? "
 				redirectText='Sign up'
-				additionalStyles={{ marginTop: '0.5rem' }}
+				additionalStyles={{ marginTop: '1.2rem' }}
 				redirectHandler={() => navigate('/register')}
 			/>
 		</>
