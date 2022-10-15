8f1b6b0b164406f6d04e9121ef18e47fc0d155cd
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-15 15:42:26 -0600

    add dummy friends for display

--- frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.jsx
+++ frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.jsx
@@ -1,4 +1,28 @@
 import { styled } from '@mui/system';
+import { FriendsListItem } from '../index';
+
+const DUMMY_FRIENDS = [
+	{
+		id: '1',
+		username: 'John',
+		isOnline: true,
+	},
+	{
+		id: '2',
+		username: 'Jane',
+		isOnline: false,
+	},
+	{
+		id: '3',
+		username: 'Jack',
+		isOnline: true,
+	},
+	{
+		id: '4',
+		username: 'Jill',
+		isOnline: false,
+	},
+];
 
 const MainContainer = styled('div')({
 	flexGrow: 1,
@@ -6,5 +30,16 @@ const MainContainer = styled('div')({
 });
 
 export const FriendsList = () => {
-	return <MainContainer></MainContainer>;
+	return (
+		<MainContainer>
+			{DUMMY_FRIENDS.map((friend) => (
+				<FriendsListItem
+					key={friend.id}
+					id={friend.id}
+					username={friend.username}
+					isOnline={friend.isOnline}
+				/>
+			))}
+		</MainContainer>
+	);
 };
