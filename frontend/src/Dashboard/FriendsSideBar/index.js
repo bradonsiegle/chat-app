27feebdadc150c290f2261271b385baa41684371
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-15 14:20:31 -0600

    add index for exports

--- frontend/src/Dashboard/FriendsSideBar/index.js
+++ frontend/src/Dashboard/FriendsSideBar/index.js
@@ -1,13 +1,6 @@
 import { AddFriendButton } from './AddFriendButton';
 import { FriendsList } from './FriendsList/FriendsList';
-import { FriendsSideBar } from './FriendsSideBar';
 import { FriendsTitle } from './FriendsTitle';
 import { PendingInvitationsList } from './PendingInvitationsList/PendingInvitationsList';
 
-export {
-	PendingInvitationsList,
-	FriendsTitle,
-	AddFriendButton,
-	FriendsList,
-	FriendsSideBar,
-};
+export { PendingInvitationsList, FriendsTitle, AddFriendButton, FriendsList };
