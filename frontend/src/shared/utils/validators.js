14116ab41daed279f1b0db7755c43d315d27b3c9
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-15 15:06:53 -0600

    add export

--- frontend/src/shared/utils/validators.js
+++ frontend/src/shared/utils/validators.js
@@ -49,7 +49,7 @@ const validateUsername = (username) => {
 	return username.length >= 3 && username.length <= 16;
 };
 
-const validateEmail = (email) => {
+export const validateEmail = (email) => {
 	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 	return emailRegex.test(String(email)?.toLowerCase());
 };
