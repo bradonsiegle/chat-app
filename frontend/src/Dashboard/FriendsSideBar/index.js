4d8bfb9047809750667967119ff5c726432c718c
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-15 15:06:45 -0600

    add export

--- frontend/src/Dashboard/FriendsSideBar/index.js
+++ frontend/src/Dashboard/FriendsSideBar/index.js
@@ -2,5 +2,12 @@ import { AddFriendButton } from './AddFriendButton';
 import { FriendsList } from './FriendsList/FriendsList';
 import { FriendsTitle } from './FriendsTitle';
 import { PendingInvitationsList } from './PendingInvitationsList/PendingInvitationsList';
+import { AddFriendDialog } from './AddFriendDialog';
 
-export { PendingInvitationsList, FriendsTitle, AddFriendButton, FriendsList };
+export {
+	PendingInvitationsList,
+	FriendsTitle,
+	AddFriendButton,
+	FriendsList,
+	AddFriendDialog,
+};
