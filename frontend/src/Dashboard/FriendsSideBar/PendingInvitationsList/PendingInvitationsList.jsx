cb34e34fd7aa032b1ee82df7fa9d41af76d9f72b
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-17 13:01:36 -0600

    add list for pending invitations

--- frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.jsx
+++ frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.jsx
@@ -1,14 +1,46 @@
 import { styled } from '@mui/system';
+import { PendingInvitationsListItem } from './PendingInvitationsListItem';
+
+const DUMMY_INVITATIONS = [
+	{
+		_id: 1,
+		senderId: {
+			username: 'John',
+			email: 'John@gmail.com',
+		},
+	},
+	{
+		_id: 2,
+		senderId: {
+			username: 'Jane',
+			email: 'Jane@gmail.com',
+		},
+	},
+	{
+		_id: 3,
+		senderId: {
+			username: 'Jack',
+			email: 'Jack@gmail.com',
+		},
+	},
+];
 
 const MainContainer = styled('div')({
 	width: '100%',
-	height: '22%',
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	overflowY: 'auto',
+	marginBottom: '1rem',
 });
 
 export const PendingInvitationsList = () => {
-	return <MainContainer></MainContainer>;
+	return (
+		<MainContainer>
+			{DUMMY_INVITATIONS.map((invitation) => (
+				<PendingInvitationsListItem
+					key={invitation._id}
+					id={invitation._id}
+					username={invitation.senderId.username}
+					email={invitation.senderId.email}
+				/>
+			))}
+		</MainContainer>
+	);
 };
