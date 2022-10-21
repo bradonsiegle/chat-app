7bcdb3c8a9832cea22a7b2ebca9e363c601dfe47
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-21 15:16:29 -0600

    add remove connected user

--- backend/serverStore.js
+++ backend/serverStore.js
@@ -1,11 +1,18 @@
-const distModules = require('react-vertical-timeline-component/dist-modules');
-
 const conenctedUsers = new Map();
 
 const addNewConnectedUser = ({ socketId, userId }) => {
 	conenctedUsers.set(socketId, { userId });
+	console.log('conenctedUsers', conenctedUsers);
+};
+
+const removeConnectedUser = (socketId) => {
+	if (conenctedUsers.has(socketId)) {
+		conenctedUsers.delete(socketId);
+	}
+	console.log('conenctedUsers', conenctedUsers);
 };
 
 module.exports = {
 	addNewConnectedUser,
+	removeConnectedUser,
 };
