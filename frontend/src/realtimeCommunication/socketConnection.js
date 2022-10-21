d428988b67e244578e5081382518e8efa3431b65
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-21 15:17:35 -0600

    add const url

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -1,16 +1,17 @@
 import io from 'socket.io-client';
+const URL = 'http://localhost:5002';
 
 let socket = null;
 
 export const connectWithSocketServer = (userDetails) => {
-	socket = io('http://localhost:5002', {
+	socket = io(URL, {
 		auth: {
 			token: userDetails.token,
 		},
 	});
 
 	socket.on('connect', () => {
-		console.log('connected');
-		console.log(socket.id);
+		console.log('connected to socket server');
+		console.log('socket id:', socket.id);
 	});
 };
