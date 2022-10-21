1b0667ea63a828e9c83dc9484188641fb2a62d02
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-21 17:43:55 -0600

    add listener for friends invitations

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -1,4 +1,7 @@
 import io from 'socket.io-client';
+import { setPendingFriendInvitations } from '../store/actions/friendsActions';
+import { store } from '../store/store';
+
 const URL = 'http://localhost:5002';
 
 let socket = null;
@@ -13,5 +16,13 @@ export const connectWithSocketServer = (userDetails) => {
 	socket.on('connect', () => {
 		console.log('connected to socket server');
 		console.log('socket id:', socket.id);
+	});
+
+	socket.on('friends-invitations', (data) => {
+		const { pendingInvitations } = data;
+		console.log('friends invitations event received');
+		console.log('pendingInvitations', pendingInvitations);
+
+		store.dispatch(setPendingFriendInvitations(pendingInvitations));
 	});
 };
