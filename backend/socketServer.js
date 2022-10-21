ef2341d2a3bb196992c9d03bb5b4d95e4a8e1f59
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-21 15:16:37 -0600

    add disconnect listener

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -1,5 +1,6 @@
 const authSocket = require('./middleware/authSocket');
 const newConnectionHandler = require('./socketHandlers/newConnectionHandler');
+const disconnectHandler = require('./socketHandlers/disconnectHandler');
 
 const registerSocketServer = (server) => {
 	const io = require('socket.io')(server, {
@@ -16,8 +17,11 @@ const registerSocketServer = (server) => {
 	io.on('connection', (socket) => {
 		console.log('a user connected');
 		console.log(socket.id);
-
 		newConnectionHandler(socket, io);
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+			disconnectHandler(socket);
+		});
 	});
 };
 
