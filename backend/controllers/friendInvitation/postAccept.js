4f55d0e0c150dd5b0a5563f278206ce0750f0cd7
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-25 16:35:56 -0600

    add logic for request accept

--- backend/controllers/friendInvitation/postAccept.js
+++ backend/controllers/friendInvitation/postAccept.js
@@ -1,5 +1,38 @@
+const FriendInvitation = require('../../models/friendInvitation');
+const User = require('../../models/user');
+const friendsUpdates = require('../../socketHandlers/updates/friends');
+
 const postAccept = async (req, res) => {
-	return res.send('postAccept');
+	try {
+		const { id } = req.body;
+
+		//Check if the invitation exists
+		const invitation = await FriendInvitation.findById(id);
+		if (!invitation) {
+			return res.status(404).json({ message: 'Invitation not found' });
+		}
+
+		const { senderId, receiverId } = invitation;
+
+		//Add to eachother's friends list
+		const sender = await User.findById(senderId);
+		const reciever = await User.findById(receiverId);
+		sender.friends.push(receiverId);
+		reciever.friends.push(senderId);
+		await sender.save();
+		await reciever.save();
+
+		//Delete the invitation
+		await FriendInvitation.findByIdAndDelete(id);
+
+		//Update pending invitations
+		friendsUpdates.updateFriendsPendingInvitations(receiverId.toString());
+
+		return res.status(200).send('Friend has been added, congratulations!');
+	} catch (error) {
+		console.log(error);
+		res.status(500).send('Something went wrong, please try again later');
+	}
 };
 
 module.exports = postAccept;
