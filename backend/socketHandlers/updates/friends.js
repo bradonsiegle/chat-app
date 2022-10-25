15433608ed194241aaeedcf83ccec32e7d3f5d24
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-25 17:01:03 -0600

    add  logic for updating friends list event

--- backend/socketHandlers/updates/friends.js
+++ backend/socketHandlers/updates/friends.js
@@ -24,4 +24,38 @@ const updateFriendsPendingInvitations = async (userId) => {
 	}
 };
 
+const updateFriends = async (userId) => {
+	try {
+		//Check for active connections of the user
+		const recieverList = serverStore.getActiveConnections(userId);
+
+		if (recieverList.length > 0) {
+			const user = await User.findById(userId, { _id: 1, friends: 1 }).populate(
+				'friends',
+				'_id username email'
+			);
+
+			if (user) {
+				const friendsList = user.friends.map((friend) => {
+					return {
+						id: friend._id,
+						username: friend.username,
+						email: friend.email,
+					};
+				});
+
+				const io = serverStore.getSocketServerInstance();
+
+				//Send the friends list to the active connections
+				recieverList.forEach((recieverSocketId) => {
+					io.to(recieverSocketId).emit('friends-list', {
+						friends: friendsList ? friendsList : [],
+					});
+				});
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
 module.exports = { updateFriendsPendingInvitations };
