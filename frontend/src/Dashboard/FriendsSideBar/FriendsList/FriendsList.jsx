5c19567a40fb28ae15e34cd724e4e3e095e9268c
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-25 17:53:16 -0600

    check for online users

--- frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.jsx
+++ frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.jsx
@@ -7,11 +7,20 @@ const MainContainer = styled('div')({
 	width: '100%',
 });
 
+const checkOnlineUsers = (friends = [], onlineUsers = []) => {
+	friends.forEach((friend) => {
+		const isOnline = onlineUsers.find((user) => user.userId === friend.id);
+		friend.isOnline = isOnline ? true : false;
+	});
+	return friends;
+};
+
 export const FriendsList = () => {
 	const friends = useSelector((state) => state.friends.friends);
+	const onlineUsers = useSelector((state) => state.friends.onlineUsers);
 	return (
 		<MainContainer>
-			{friends.map((friend) => (
+			{checkOnlineUsers(friends, onlineUsers).map((friend) => (
 				<FriendsListItem
 					key={friend.id}
 					id={friend.id}
