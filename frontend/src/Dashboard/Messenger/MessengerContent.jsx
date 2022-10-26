da0cb907700d7c44786aa1f0a82133ba547d0530
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-26 17:52:20 -0600

    fix styles

--- frontend/src/Dashboard/Messenger/MessengerContent.jsx
+++ frontend/src/Dashboard/Messenger/MessengerContent.jsx
@@ -4,6 +4,10 @@ import { Messages } from './Messages/Messages';
 import { NewMessageInput } from './NewMessageInput';
 
 const Wrapper = styled('div')({
+	height: '100%',
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'column',
 	flexGrow: 1,
 });
 
