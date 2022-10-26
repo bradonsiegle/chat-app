5e9aab1a998527b819f956bca9d5c35dfe7643ac
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-25 18:11:59 -0600

    add chat slice

---
+++ frontend/src/store/reducers/chatReducer.js
@@ -0,0 +1,26 @@
+import { chatActions } from '../actions/chatActions';
+
+const initialState = {
+	chosenChatDetails: null,
+	chatType: null,
+	messages: [],
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case chatActions.SET_CHOSEN_CHAT_DETAILS:
+			return {
+				...state,
+				chosenChatDetails: action.payload,
+				chatType: action.chatType,
+				messages: [],
+			};
+		case chatActions.SET_MESSAGES:
+			return {
+				...state,
+				messages: action.payload,
+			};
+		default:
+			return state;
+	}
+};
