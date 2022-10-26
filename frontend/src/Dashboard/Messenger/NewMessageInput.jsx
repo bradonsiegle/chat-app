f757863e4cbd7992b5256d7824f48df20b89bb22
Bradon Siegle <bradonsiegle@gmail.com>
2022-10-26 17:52:38 -0600

    add new message input structure

--- frontend/src/Dashboard/Messenger/NewMessageInput.jsx
+++ frontend/src/Dashboard/Messenger/NewMessageInput.jsx
@@ -1,3 +1,60 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { styled } from '@mui/system';
+
+const MainContainer = styled('div')({
+	height: '4rem',
+	width: '100%',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+});
+
+const Input = styled('input')({
+	width: '96%',
+	height: '3.4rem',
+	padding: '0 0.8rem',
+	marginBottom: '1.5rem',
+	backgroundColor: '#2f3136',
+	border: 'none',
+	color: 'white',
+	fontSize: '1rem',
+	borderRadius: '0.5rem',
+	':focus': {
+		outline: 'none',
+		border: '1px solid #7289da',
+	},
+});
+
 export const NewMessageInput = () => {
-	return <div>NewMessageInput</div>;
+	const [message, setMessage] = useState('');
+
+	const chosenChatDetails = useSelector(
+		(state) => state.chat.chosenChatDetails
+	);
+
+	const handleMessageValueChange = (e) => {
+		setMessage(e.target.value);
+	};
+
+	const handleKeyPressed = (e) => {
+		if (e.key === 'Enter') {
+			handleSendMessage();
+		}
+	};
+
+	const handleSendMessage = () => {
+		console.log('Sending message: ', message);
+	};
+
+	return (
+		<MainContainer>
+			<Input
+				placeholder={`Message @${chosenChatDetails?.name}`}
+				value={message}
+				onChange={handleMessageValueChange}
+				onKeyDown={handleKeyPressed}
+			/>
+		</MainContainer>
+	);
 };
